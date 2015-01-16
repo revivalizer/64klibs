d29663d08c03e00b6fa7544cdb17590282418a4f
Ralph Brorsen <ralph@revivalizer.dk>
2015-01-16 22:18:45 +0100

    Included memset in startup project.

--- startup/main.cpp
+++ startup/main.cpp
@@ -25,3 +25,12 @@ extern "C" int __cdecl _purecall(void)
     return 0;  
 }  
 
+#pragma function(memset)
+void * __cdecl memset(void *pTarget, int value, size_t cbTarget) {
+	unsigned char *p = static_cast<unsigned char *>(pTarget);
+	while (cbTarget-- > 0) {
+		*p++ = static_cast<unsigned char>(value);
+	}
+	return pTarget;
+}
+
