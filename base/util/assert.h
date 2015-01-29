f1ebe96540cc38da8e4848e6a7a6c2f030277254
Ralph Brorsen <ralph@revivalizer.dk>
2015-01-29 22:57:46 +0100

    Add ZBREAK() macro, which simulates a breakpoint, entering the debugger, hopefully. Also, push/pop warning disables in assert, seems more safe.

--- base/util/assert.h
+++ base/util/assert.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #ifdef ENABLE_ZASSERT
+	#pragma warning(push)
 	#pragma warning(disable: 4793)
 	#pragma warning(disable : 4800)
 
@@ -8,10 +9,16 @@
 		#define ZASSERT(exp) if( !static_cast<bool>(exp) ) { _asm{ int 3 } }
 	#endif
 
-	#pragma warning(default: 4793)
-	#pragma warning(default: 4800)
+	#ifndef ZBREAK
+		#define ZBREAK() { _asm{ int 3 } }
+	#endif
+
+	#pragma warning(pop)
 #else
 	#ifndef ZASSERT
 		#define ZASSERT(exp)
+	#endif
+	#ifndef ZBREAK
+		#define ZBREAK()
 	#endif
 #endif
